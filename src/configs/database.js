15507c5161f044022d6d7405bb4f2feaf35070af
Yofriadi Yahya <yofriadiyahya@gmail.com>
2018-01-17 19:00:40 +0700

    Ready to deploy

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const connection = mongoose.connection
 require('dotenv').config()
 
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(process.env.MLAB_URI)
 mongoose.Promise = global.Promise
 
 module.exports = {mongoose, connection}
